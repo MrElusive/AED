7d84d16a3b964ba035dcb3161bc85276da131bd6
Clayton Lemons <MrElusive@gmail.com>
2013-03-12 12:27:52 -0600

    Feature Extractor
    Added a test arff file: faces_50i-49d.arff
    The pattern for the file name is this: 50i refers to 50 instances, 49d refers to 49 dimensions.
    
    Finished the feature_extractor.py program.  Run python ./feature_extractor.py --help to see the help options.
    
    Modified image_to_csv.py program to allow a .pic file to be used for image preferences.  This will allow us to go through each image file and choose whether we want to include it or leave it out of our final data set.  The .pic file keeps track of our preference so we don't have to repeat the process everytime.

--- feature_extractor/feature_extractor.py
+++ feature_extractor/feature_extractor.py
@@ -15,149 +15,90 @@ from optparse import OptionParser
 parser = OptionParser(description=__doc__)
 
 parser.add_option("-f", dest="filename", default = None, help="filename of the data")
-parser.add_option("-t", dest="target", default = None, help="target class name")
-parser.add_option("-r", dest="ratio", default = 0.1, type=float, help="ratio of target displayed")
-parser.add_option("-s", dest="show", default=True, action='store_true', help="show new dataset")
-parser.add_option("-v", dest="verbose", default=False, action='store_true', help="show all intermediate structures")
-parser.add_option("-a", dest="algorithm", default="lle", help="set algorithm")
-parser.add_option("-k", dest="k", default=10, type=int, help="number of nearest neighbors to use")
+parser.add_option("-s", dest="show", default=False, action='store_true', help="show first two dimensions of new dataset")
+parser.add_option("-a", dest="algorithm", default="lle", help="set algorithm [lle, hlle]")
+parser.add_option("-k", dest="k", type=int, help="number of nearest neighbors to use")
 parser.add_option("-d", dest="outputDimensions", default=2, type=int, help="number of dimensions to output")
+parser.add_option("-o", dest="outputFile", default="output.arff", help="the file name for the arff output")
+parser.add_option("-n", dest="numberOfInstances", help="the number of instances to use from the dataset")
 
 options, args = parser.parse_args()
 
-if options.filename == None:
-	IMAGE_BASE_DIR = "../test_images/%s"
-	OUTPUT_DIR = "../arff"
-	OUTPUT_FILE = "myImages.arff"
+COLORS = ["r","b","g","y","m","c","b"]
+LABELS = ["happy", "sad", "angry", "surprised", "scared", "disgusted", "neutral"]
 
-	IMAGE_SUB_DIRS = ["happy"]#, "sad", "angry", "neutral", "surprised", "scared"]
+def generateARFF(result, labels, output):
+	output.write("@RELATION emotion\n\n")
 
+	for i in range(len(result[0,:])):
+		output.write("@ATTRIBUTE Dimension_%s CONTINUOUS\n" % i)	
 
-	imageFiles = []
-	for subDir in IMAGE_SUB_DIRS:
-		imageDir = IMAGE_BASE_DIR % subDir
-		imageFiles.extend([path.join(imageDir, imageFile) for imageFile in os.listdir(imageDir) if path.isfile(path.join(imageDir, imageFile))])
-
-
-	hc = cv.Load("haarcascade_frontalface_default.xml")
-
-	dataFile = open("data.csv", "w")
-	for i in range(100 * 100):
-		dataFile.write(str(i))
-		if i < 100 * 100 - 1:
-			dataFile.write(", ")
-
-	dataFile.write("\n")
-
-	for imageFile in imageFiles:
-
-		# This code was taken from http://opencv.willowgarage.com/documentation/python/objdetect_cascade_classification.html
-
-		# Load data for detecting faces
-		image = cv.LoadImage(imageFile, 0)
-
-		# Grab faces
-		faces = cv.HaarDetectObjects(image, hc, cv.CreateMemStorage())
-
-		cv.NamedWindow('final-result')
-
-		# Crop faces
-		for (x, y, w, h), n in faces:
-			cropped = cv.CreateImage((w, h), 8, 0)
-			cv.Copy(cv.GetSubRect(image, (x, y, w, h)), cropped)
-			imageStats = cv.AvgSdv(cropped)
-			s2n = imageStats[0][0] / imageStats[1][0]
-			
-			if imageStats[1][0] > 15.0:
-				print (x, y, w, h)
-				print imageStats
-				print s2n
-
-				resized = cv.CreateImage((100, 100), 8, 0)
-				cv.Resize(cropped, resized);
-
-				if s2n < 5:
-					smoothed = cv.CreateImage((100, 100), 8, 0)
-					cv.Smooth(resized, smoothed, 3, 3)
-				else:
-					smoothed = resized
-
-				edged = cv.CreateImage((100, 100), 32, 0)
-				cv.Laplace(smoothed, edged, 3)
-
-				scaled = cv.CreateImage((100, 100), 8, 0)
-				cv.ConvertScaleAbs(edged, scaled)
-
-				finalResult = scaled
-
-				cv.ShowImage('final-result', finalResult)
-				while True:
-					keyPressed = chr(cv.WaitKey(0) & 255)
-		
-					if keyPressed == 'n':
-						print "Image not accepted"
-						break
-					elif keyPressed == 'y':
-						print "Image accepted"
-
-						numpy.savetxt(dataFile, numpy.concatenate(numpy.asarray(cv.GetMat(finalResult)))[None], fmt="%d", delimiter=", ")
-						
-						break
-					else:
-						print "Press 'y' or 'n'"
-
-else:
-	print "Loading data set"
-	# load dataset
-	dataset = numpy.loadtxt(open(options.filename, 'rb'), delimiter=",", skiprows=1)#Dataset(options.filename)
-
-	xlabel = 'first eigen vector'
-	ylabel = 'second eigen vector'
-
-	if options.algorithm == 'lle':
-		print "Running LLE"
-		print str(dataset)
-		lle = mdp.nodes.LLENode(options.k, output_dim=options.outputDimensions)
-		result = lle(dataset)
-
-		pylab.title("After LLE")
-		xlabel = 'dim 1'
-		ylabel = 'dim 2'
-		x, y = result[:,0], result[:,1]
-
-	elif options.algorithm == 'hlle':
-		hlle = mdp.nodes.HLLENode(options.k, output_dim=options.outputDimensions)(dataset)
-		x, y = hlle[:,0], hlle[:,1]
-
-	pylab.plot(x, y, '.')
-	pylab.xlabel(xlabel)
-	pylab.ylabel(ylabel)
-	pylab.show() 
-		
-	# print new dataset
-	if options.show:
-		for i,(a,b) in enumerate(zip(x,y)):
-			print "%s, %s" % (a, b)
+	output.write("\n@DATA\n")
+	for i in range(len(result[:,0])):
+		output.write("%s, %s\n" % (", ".join(map(str, result[i])), LABELS[int(labels[i, 0])]))
 
 
-'''
-with open(path.join(OUTPUT_DIR, OUTPUT_FILE), 'w') as outputFile:
-	for i in range(100 * 100):
-		outputFile.write(str(i))
-		if i != 100 * 100 - 1:
-			outputFile.write(", ")
+print "Loading data set..."
+dataset = numpy.loadtxt(open(options.filename, 'rb'), delimiter=",")
+print "Data set loaded.\n"
+
+# Split out label column
+labels = dataset[:,[len(dataset[0]) - 1]]
+
+dataset = dataset[:,0:len(dataset[0]) - 1]
+
+originalNumberOfInstances = len(dataset[:,0])
+
+if options.numberOfInstances:
+	labels = labels[0:int(options.numberOfInstances), :]
+	dataset = dataset[0:int(options.numberOfInstances), :]
+
+# The default value for k is the number of instances minus 2, the maximum possible value of k
+if not options.k:
+	options.k = len(dataset[:,0]) - 1
+
+
+print "Using %d nearest neighbors." % options.k
+print "Using %d out of %d instances." % (len(dataset[:,0]), originalNumberOfInstances) 
+print "Source Dimensionality: %d; Target Dimensionality %d.\n" % (len(dataset[0,:]), options.outputDimensions)
+
+if options.algorithm == 'lle':
+	print "Running LLE..."
+	lle = mdp.nodes.LLENode(options.k, output_dim=options.outputDimensions)
+	result = lle(dataset)	
+
+elif options.algorithm == 'hlle':
+	print "Running HLLE..."
+	hlle = mdp.nodes.HLLENode(options.k, output_dim=options.outputDimensions)(dataset)
+	result = hlle(dataset)
+
+print "Finished running.\n"
+print "Final Dimensionality: %d.\n" % len(result[0,:])
+
+print "Generating ARFF files..."
+with open(options.outputFile, "w") as output:
+	generateARFF(result, labels, output)	
+print "Finished generating ARFF files.\n"
+
+# Show the first two dimensions of the new data set.
+if options.show:
+	print "First Two Dimensions of Final Result:"
+	x, y = numpy.append(result[:,0:1], labels, 1), numpy.append(result[:,1:2], labels, 1)
+
+	for i, (a, b) in enumerate(zip(x, y)):
+		print "%s, %s" % (a, b)
+
+	pylab.title("Result")
+
+	xlabel = 'dim 1'
+	ylabel = 'dim 2'
+
+	for i in range(0, 7):
+		x_points = x[x[:,1] == i]
+		y_points = y[y[:,1] == i]
+
+		pylab.scatter(x_points[:,0], y_points[:,0], c=COLORS[i])
+		pylab.xlabel(xlabel)
+		pylab.ylabel(ylabel)
 	
-	outputFile.write("\n")
-
-	for imageFile in imageFiles:
-		image = Image.open(imageFile).convert('L').resize((100, 100))
-		
-		data = list(image.getdata())
-
-		for i in range(len(data)):
-			outputFile.write(str(data[i]))
-			if i != len(data) - 1:
-				outputFile.write(", ")
-		
-		outputFile.write("\n")
-'''
+	pylab.show()
--- feature_extractor/image_to_csv.py
+++ feature_extractor/image_to_csv.py
@@ -1,41 +1,159 @@
-from PIL import Image
+#!/usr/bin/python
 import os
+import pickle
 from os import path
+from optparse import OptionParser
 
-IMAGE_DIR = "../test_images/%s"
-HAPPY_DIR = IMAGE_DIR % "happy"
-SAD_DIR = IMAGE_DIR % "sad"
-ANGRY_DIR = IMAGE_DIR % "angry"
-NEUTRAL_DIR = IMAGE_DIR % "neutral"
-SURPRISED_DIR = IMAGE_DIR % "surprised"
-SCARED_DIR = IMAGE_DIR % "scared"
+import cv
+import numpy
+import yaml
 
-OUTPUT_DIR = "./csv_output"
+HC = cv.Load("haarcascade_frontalface_default.xml")
 
-imageFiles = []
-imageFiles.extend([path.join(HAPPY_DIR, imageFile) for imageFile in os.listdir(HAPPY_DIR) if path.isfile(path.join(HAPPY_DIR, imageFile))])
-imageFiles.extend([path.join(SAD_DIR, imageFile) for imageFile in os.listdir(SAD_DIR) if path.isfile(path.join(SAD_DIR, imageFile))])
-imageFiles.extend([path.join(ANGRY_DIR, imageFile) for imageFile in os.listdir(ANGRY_DIR) if path.isfile(path.join(ANGRY_DIR, imageFile))])
-imageFiles.extend([path.join(NEUTRAL_DIR, imageFile) for imageFile in os.listdir(NEUTRAL_DIR) if path.isfile(path.join(NEUTRAL_DIR, imageFile))])
-imageFiles.extend([path.join(SURPRISED_DIR, imageFile) for imageFile in os.listdir(SURPRISED_DIR) if path.isfile(path.join(SURPRISED_DIR, imageFile))])
-imageFiles.extend([path.join(SCARED_DIR, imageFile) for imageFile in os.listdir(SCARED_DIR) if path.isfile(path.join(SCARED_DIR, imageFile))])
+def createUniqueOutputFile(outputFile):
+	newFile = outputFile
+	i = 1
 
-with open(path.join(OUTPUT_DIR, "images.csv"), 'w') as outputFile:
-	for i in range(100 * 100):
-		outputFile.write(str(i))
-		if i != 100 * 100 - 1:
-			outputFile.write(", ")
+	while (path.exists(newFile)):
+		(pathHead, pathTail) = path.split(outputFile)
+		(baseFileName, ext) = path.splitext(path.basename(pathTail))
+
+		newFile = path.join(pathHead, "%s (%d)%s" % (baseFileName, i, ext))
+		i += 1
+		
+	return newFile
+
+def parseParameters(parameterFile):
+	if parameterFile:
+		with open(parameterFile) as parameters:
+			return yaml.safe_load(parameters)
+	else:
+		return {
+			'stddev': 15.0,
+			'final-image-size': (250, 250),
+			'signal-to-noise': 1,
+			'smooth-type': cv.CV_GAUSSIAN,
+			'param1': 3,
+			'param2': 0,
+			'param3': 0,
+			'param4': 0,
+			'aperture-size': 3
+		} 
+
+# This code is based off the tutorial at http://opencv.willowgarage.com/documentation/python/objdetect_cascade_classification.html
+def processImageFile(imageFile, output, param, verbose, imagePreferenceFile):
+	global imagePreference
 	
-	outputFile.write("\n")
+	# Load data for detecting faces
+	image = cv.LoadImage(imageFile, 0)
 
-	for imageFile in imageFiles:
-		image = Image.open(imageFile).convert('L').resize((100, 100))
-		
-		data = list(image.getdata())
+	# Grab faces
+	faces = cv.HaarDetectObjects(image, HC, cv.CreateMemStorage())
 
-		for i in range(len(data)):
-			outputFile.write(str(data[i]))
-			if i != len(data) - 1:
-				outputFile.write(", ")
-		
-		outputFile.write("\n")
+	cv.NamedWindow('Preview Result')
+
+	# Process faces
+	for (x, y, w, h), n in faces:
+		cropped = cv.CreateImage((w, h), 8, 0)
+		cv.Copy(cv.GetSubRect(image, (x, y, w, h)), cropped)
+
+		imageStats = cv.AvgSdv(cropped)
+
+		mean = imageStats[0][0]
+		stdDev = imageStats[1][0]
+		signalToNoise = mean / stdDev
+	
+		# If the image has a standard deviation above a given threshold, then consider it to be a real face.
+		if stdDev > param['stddev']:
+			if (verbose):
+				print (x, y, w, h)
+				print imageStats
+				print signalToNoise
+
+			resized = cv.CreateImage(param['final-image-size'], 8, 0)
+			cv.Resize(cropped, resized);
+
+			# Only smooth an image if the signal-to-noise ratio is below agiven threshold
+			if signalToNoise < param['signal-to-noise']:
+				smoothed = cv.CreateImage(param['final-image-size'], 8, 0)
+				cv.Smooth(resized, smoothed, param['smooth-type'], param['param1'], param['param2'], param['param3'], param['param4'])
+			else:
+				smoothed = resized
+
+			# Discover the edges in the image
+			edged = smoothed #cv.CreateImage(param['final-image-size'], 32, 0)
+			#cv.Laplace(smoothed, edged, param['aperture-size'])
+
+			# Scale in terms of bit-depth, not image size
+			scaled = cv.CreateImage(param['final-image-size'], 8, 0)
+			cv.ConvertScaleAbs(edged, scaled)
+
+			finalResult = scaled
+
+			cv.ShowImage('Preview Result', finalResult)
+			while True:
+				if (imageFile, n) in imagePreference:
+					keyPressed = imagePreference[(imageFile, n)]
+				else:
+					keyPressed = chr(cv.WaitKey(0) & 255)
+	
+				if keyPressed == 'n' or keyPressed == 'N':
+					print "Image skipped!"
+					break
+				elif keyPressed == 'y' or keyPressed == 'Y':
+					imageLabel = int(path.basename(imageFile)[0:1])
+					imageArray = numpy.asarray(cv.GetMat(finalResult))
+					imageArray = numpy.concatenate(imageArray)
+					imageArray = numpy.append(imageArray, imageLabel)
+					numpy.savetxt(output, imageArray[None], fmt="%d", delimiter=", ")
+					print "Image added!"
+					
+					break
+				else:
+					print "Please enter either 'y'/'Y' or 'n'/'N'"
+
+
+			imagePreference[(imageFile, n)] = keyPressed 
+			pickle.dump(imagePreference, file(imagePreferenceFile, 'w'))
+
+parser = OptionParser(description=__doc__)
+
+parser.add_option("-i", dest="imageFolder", default="../images", help="image folder")
+parser.add_option("-o", dest="outputCSVFile", default="output.csv", help="output csv file")
+parser.add_option("-v", dest="verbose", default=False, action='store_true', help="show verbose output")
+parser.add_option("-c", dest="clobber", default=False, action='store_true', help="always clobber the output file")
+parser.add_option("-p", dest="parameterFile", default=None, help="designate the parameters for processing the image")
+parser.add_option("-r", dest="imagePreferenceFile", default=None, help="keeps track of which images are accepted and which are not")
+
+options, args = parser.parse_args()
+
+imageDirWorkList = [options.imageFolder]
+outputFile = options.outputCSVFile
+
+if not options.clobber:
+	outputFile = createUniqueOutputFile(outputFile)
+
+param = parseParameters(options.parameterFile)
+
+if options.imagePreferenceFile and path.exists(options.imagePreferenceFile):
+	try:
+		imagePreference = pickle.load(file(options.imagePreferenceFile))
+	except pickle.UnpicklingError:
+		print "Unable to unpickle the file '%s'." % options.imagePreferenceFile
+		exit(1)
+else:
+	imagePreference = {}
+
+with open(outputFile, 'w') as output:
+	while imageDirWorkList:
+		imageDir = imageDirWorkList.pop()
+
+		for dirObject in os.listdir(imageDir):
+			dirObject = path.join(imageDir, dirObject)
+			if path.isdir(dirObject):
+				imageDirWorkList.append(dirObject)
+			elif path.isfile(dirObject):
+				processImageFile(dirObject, output, param, options.verbose, options.imagePreferenceFile)
+			else:
+				print "Encountered something that is neither a file or directory: %s" % dirObject
+
